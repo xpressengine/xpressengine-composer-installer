5ba4ca5df5af629088763af1cbfb3ad1567a626a
xedev <developers@xpressengine.com>
2016-06-13 17:03:33 +0900

    plugins list 관리기능 추가

--- src/Xpressengine/Installer/XpressengineInstaller.php
+++ src/Xpressengine/Installer/XpressengineInstaller.php
@@ -13,8 +13,13 @@
  */
 namespace Xpressengine\Installer;
 
-use Composer\Package\PackageInterface;
+use Composer\Composer;
+use Composer\Installer\BinaryInstaller;
 use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Composer\Util\Filesystem;
 
 /**
  * This class is extend composer installer for XpressEngine plugins.
@@ -28,6 +33,29 @@ use Composer\Installer\LibraryInstaller;
  */
 class XpressengineInstaller extends LibraryInstaller
 {
+
+    public static $changed = [];
+
+    /**
+     * Initializes library installer.
+     *
+     * @param IOInterface $io
+     * @param Composer $composer
+     * @param string $type
+     * @param Filesystem $filesystem
+     * @param BinaryInstaller $binaryInstaller
+     */
+    public function __construct(IOInterface $io, Composer $composer, $type = 'library', Filesystem $filesystem = null, BinaryInstaller $binaryInstaller = null)
+    {
+        static::$changed = [
+            'installed' => [],
+            'updated' => [],
+            'uninstalled' => [],
+        ];
+
+        parent::__construct($io, $composer, $type, $filesystem, $binaryInstaller);
+    }
+
     /**
      * Directory in which the plugin is installed
      *
@@ -36,6 +64,7 @@ class XpressengineInstaller extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
+
         list(, $packageName) = explode('/', $package->getPrettyName());
 
         return 'plugins/' . $packageName;
@@ -53,4 +82,24 @@ class XpressengineInstaller extends LibraryInstaller
     {
         return 'xpressengine-plugin' === $packageType;
     }
+
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        parent::install($repo, $package);
+        static::$changed['installed'][$package->getName()] = $package->getPrettyVersion();
+    }
+
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    {
+        parent::update($repo, $initial, $target);
+        static::$changed['updated'][$target->getName()] = $target->getPrettyVersion();
+    }
+
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        parent::uninstall($repo, $package);
+        static::$changed['uninstalled'][$package->getName()] = $package->getPrettyVersion();
+    }
+
+
 }
--- src/Xpressengine/Installer/XpressengineInstallerPlugin.php
+++ src/Xpressengine/Installer/XpressengineInstallerPlugin.php
@@ -14,8 +14,16 @@
 namespace Xpressengine\Installer;
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\Event;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\InstallerEvent;
+use Composer\Installer\InstallerEvents;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFormatter;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
 
 /**
  * This class is register composer plugin.
@@ -27,18 +35,135 @@ use Composer\Plugin\PluginInterface;
  * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
  * @link        http://www.xpressengine.com
  */
-class XpressengineInstallerPlugin implements PluginInterface
+class XpressengineInstallerPlugin implements PluginInterface, EventSubscriberInterface
 {
+
+    protected $path;
+
+    protected $enabled;
+
     /**
      * Apply plugin modifications to composer
      *
      * @param Composer    $composer composer instance
      * @param IOInterface $io       IO instance
      * @return void
-     */
+    */
     public function activate(Composer $composer, IOInterface $io)
     {
+
+        if($composer->getPackage()->getName() !== 'xpressengine/xpressengine') {
+            $this->enabled = false;
+            return;
+        } else {
+            $this->enabled = true;
+        }
+
         $installer = new XpressengineInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            /*InstallerEvents::PRE_DEPENDENCIES_SOLVING => 'preDependencySolve',*/
+            InstallerEvents::POST_DEPENDENCIES_SOLVING => 'postDependencySolve',
+            ScriptEvents::PRE_INSTALL_CMD => 'onInstallUpdateOrDump',
+            ScriptEvents::PRE_UPDATE_CMD => 'onInstallUpdateOrDump',
+            ScriptEvents::POST_INSTALL_CMD => 'postInstallOrUpdate',
+            ScriptEvents::POST_UPDATE_CMD => 'postInstallOrUpdate',
+        );
+    }
+
+    public function onInstallUpdateOrDump(Event $event)
+    {
+        if(!$this->enabled) {
+            return;
+        }
+
+        // 파일 경로를 읽고, plugin-update 모드인지 검사.
+        // plugin-update 모드인데 packages가 xpressengine-plugin이 아니면 Exception
+        $data = $this->getPluginComposerData($event);
+
+        $mode = array_get($data, 'xpressengine-plugin.mode');
+        if($mode !== 'plugins-fixed') {
+            $packages = $GLOBALS['argv'][$GLOBALS['argc'] - 1];
+            if(strpos($packages, 'xpressengine-plugin') !== 0) {
+                throw new \Exception("Xpressengine installer: check file[".$this->path."]. this file is not correct");
+            }
+        }
+
+    }
+
+
+    public function postDependencySolve(InstallerEvent $event)
+    {
+        if(!$this->enabled) {
+            return;
+        }
+
+        $extra = $event->getComposer()->getPackage()->getExtra();
+
+        $uninstall = array_get($extra, 'xpressengine-plugin.uninstall', []);
+        foreach ($event->getOperations() as $operation) {
+            /** @var UpdateOperation $operation */
+            if (is_subclass_of($operation, UpdateOperation::class) || is_subclass_of($operation, InstallOperation::class)) {
+                $target = $operation->getInitialPackage();
+                if(in_array($target->getName(), $uninstall)) {
+                    throw new \Exception('Xpressengine installer: To install or update the package requested to delete is not allowed.', 66);
+                }
+            }
+        }
+    }
+
+    public function postInstallOrUpdate(Event $event)
+    {
+        if(!$this->enabled) {
+            return;
+        }
+
+        $extra = $event->getComposer()->getPackage()->getExtra();
+
+        $path = $extra['xpressengine-plugin']['path'];
+
+        $data = json_decode(file_get_contents($path));
+
+        $data->extra->{"xpressengine-plugin"}->changed = XpressengineInstaller::$changed;
+
+        $dataJson = json_encode($data);
+
+        $dataJson = JsonFormatter::format($dataJson, true, true);
+
+        file_put_contents($path, $dataJson);
+    }
+
+    /**
+     * getPluginComposerData
+     *
+     * @param Event $event
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function getPluginComposerData(Event $event)
+    {
+        $extra = $event->getComposer()->getPackage()->getExtra();
+
+        if (!isset($extra['xpressengine-plugin']['path'])) {
+            throw new \Exception('xpressengine-installer: extra > xpressengine-plugin > path is needed.');
+        }
+
+        $path = $extra['xpressengine-plugin']['path'];
+        $this->path = $path;
+
+        if (!file_exists($path)) {
+            throw new \Exception("file [$path] not exists.");
+        }
+
+        return json_decode(file_get_contents($path), true);
+    }
+
 }
